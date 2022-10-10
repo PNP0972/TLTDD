beec28261a296929b446de8217136298212c5f34
PNP0972 <112404450+PNP0972@users.noreply.github.com>
2022-10-10 23:33:13 +0700

    app tong hop

---
+++ GK/app/src/main/java/caidat/dmt/gk/Profile.java
@@ -0,0 +1,26 @@
+package caidat.dmt.gk;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Profile extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_profile,container,false);
+        return view;
+    }
+
+
+
+}
